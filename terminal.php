1448ecefd67220000bb4789d264a180d55d11875
dmanchev <d.manchev@icn.bg>
2014-11-09 00:28:59 +0200

    terminal update

--- terminal.php
+++ terminal.php
@@ -1,10 +1,39 @@
 <?php
 header('Content-Type: application/json');
-$request = file_get_contents('php://input');
+$command = htmlspecialchars(file_get_contents('php://input'));
 $response = new StdClass();
 
-$response = [
-    'error' => "Unknown command"
-];
+if(empty($command)) {
+    $error = true;
+} else {
+    switch($command) {
+        case "arp -a":
+            $result = "
+? (192.168.0.1) ivan-pc on en0 ifscope [ethernet]
+? (192.168.0.100) gov-pc on en0 ifscope permanent [ethernet]
+? (192.168.0.102) sec-pc on en0 ifscope [ethernet]
+? (192.168.0.107) admin-pc on en0 ifscope [ethernet]
+? (192.168.0.110) backup on en0 ifscope [ethernet]
+? (192.168.0.115) sec2 on en0 ifscope [ethernet]
+? (192.168.0.255) broadcast on en0 ifscope [ethernet]
+            ";
+            break;
+        case "ssh 192.168.0.107":
+            $result = "sshed";
+            break;
+        default:
+            $error = true;
+    }
+}
+
+if(isset($error)) {
+    $response = [
+        'error' => "Unknown command"
+    ];
+} else {
+    $response = [
+        'response' => $result
+    ];
+}
 
 echo json_encode($response);
