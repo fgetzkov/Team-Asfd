276f1d9d6b22edfb1aa8e23b6b174f9cbc641d2b
dmanchev <d.manchev@icn.bg>
2014-11-09 02:54:55 +0200

    terminal update

--- terminal.php
+++ terminal.php
@@ -3,6 +3,7 @@ session_start();
 header('Content-Type: application/json');
 $command = htmlspecialchars(file_get_contents('php://input'));
 $response = new StdClass();
+$student = "dmanchev";
 
 if(empty($command)) {
     $error = true;
@@ -22,12 +23,50 @@ if(empty($command)) {
         case "ssh 192.168.0.107":
             $result = "sshed";
             break;
+        case "ssh 129.124.65.8 -u root -p f34gbw2fs":
+            $result = "sshed2";
+            break;
         case "cat ftp.txt":
             $result = "
-ftp server: 129.124.65.8
-user: asshole
-pass: GOD
+ftp server: cleverguard.com
+user: susi@cleverguard.com
+pass: KN7]JL1uCHsT
             ";
+            break;
+        case "cat /usr/var/www/public_html/config.php":
+            $result = <<<'EOT'
+<?php
+$host = "localhost";
+$user = "susi";
+$password = "c32f24fv";
+$db = "susidb";
+?>
+EOT;
+            break;
+        case "mysql -u susi -p c32f24fv":
+            $result = "mysqled";
+            break;
+        case "USE susidb;":
+            $result = "Database changed";
+            break;
+        case "show tables;":
+            $result = "
++-----------------------+
+| Tables_in_susidb      |
++-----------------------+
+| students              |
+| exams                 |
+| admins                |
+| options               |
+| subjects              |
+| logs                  |
+| roles                 |
++-----------------------+
+7 rows in set (0.00 sec)
+            ";
+            break;
+        case "UPDATE exams SET passed = 1 WHERE student = $student;":
+            $result = "Query OK, 7 rows affected (0.01 sec)";
             break;
         default:
             $error = true;
